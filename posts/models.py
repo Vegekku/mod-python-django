e098f95c96884b096ea767df5cefb221a3d68e16
Vegekku <vegekku@gmail.com>
2019-05-18 21:14:48 +0200

    Created category & relationship with Post

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,8 @@
+from datetime import datetime
+
 from django.db import models
+
+from categories.models import Category
 
 
 class Post(models.Model):
@@ -8,6 +12,9 @@ class Post(models.Model):
     body = models.TextField()
     media = models.URLField(null=True, blank=True)
     create_date = models.DateTimeField(auto_now_add=True)
-    publish_date = models.DateTimeField(auto_now_add=True)
+    publish_date = models.DateTimeField(default=datetime.now)
     modification_date = models.DateTimeField(auto_now=True)
-    # categories = model
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.title

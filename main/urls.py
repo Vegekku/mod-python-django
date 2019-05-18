3a7a27e9513adc3006279853893ac0082ad72120
Vegekku <vegekku@gmail.com>
2019-05-19 01:01:51 +0200

    Register posts

--- main/urls.py
+++ main/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from posts.views import latest_posts, post_detail
+from posts.views import latest_posts, post_detail, create_post
 from users.views import login, logout
 
 urlpatterns = [
@@ -25,6 +25,7 @@ urlpatterns = [
     path('login', login, name='login'),
     path('logout', logout, name='logout'),
     # Posts
+    path('posts/new', create_post, name='create_post'),
     path('posts/<int:pk>', post_detail, name='post_detail'),
     path('', latest_posts, name='latest_posts'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,8 @@
+from django.contrib import messages
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 
+from posts.forms import PostForm
 from posts.models import Post
 
 
@@ -22,3 +24,17 @@ def post_detail(request, pk):
     html = render(request, 'posts/detail.html', context)
 
     return HttpResponse(html)
+
+
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            new_post = form.save()
+            messages.success(request, 'Post created successfully with ID {0}'.format(new_post.pk))
+            form = PostForm()
+    else:
+        form = PostForm()
+
+    context = {'form': form}
+    return render(request, 'posts/new.html', context)
